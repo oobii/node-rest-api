78c73c531454e6ca71981fcc80240309ed9336e2
oobii <vvayfarer@yandex.com>
2020-11-06 18:07:15 -0500

    added reading from json file

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import {people} from "./people"
+import { promises as fs} from 'fs';
+import {people} from "./people";
 
 // to run: npx babel-node src/server.js
 
@@ -13,12 +14,20 @@ app.get("/people", (req, res) => {
     res.json(people);
 });
 
+app.get("/file-data", async (req, res) => {
+
+    let data = await fs.readFile(__dirname + "/" + "people-data.json");
+    let people = JSON.parse(data);
+    res.json(people);
+});
+
 app.get("/people/:name", (req, res) => {
 
     let {name} = req.params;
     console.log(name)
 
     let person = people.find((x) => {return x.name === name});
+    //let person = people.find(x => x.name === name);
     res.json(person);
 });
 
